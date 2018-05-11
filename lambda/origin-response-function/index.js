6d5a19b89de1b2e088149be2fe8517d74063d176
Antonio Coppola <antonio@evemattress.co.uk>
2018-05-11 17:48:03 +0100

    working resize

--- lambda/origin-response-function/index.js
+++ lambda/origin-response-function/index.js
@@ -7,51 +7,106 @@ const S3 = new AWS.S3({
 const Sharp = require('sharp');
 
 // set the S3 and API GW endpoints
-const BUCKET = 'test-imageresize-eve';
-const URL = 'process.env.URL';
-const ALLOWED_DIMENSIONS = new Set();
+const BUCKET = process.env.BUCKET;
+const URL = process.env.URL;
 
-if (process.env.ALLOWED_DIMENSIONS) {
-  const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
-  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
-}
+// set allowed sizes
+// const ALLOWED_DIMENSIONS = new Set();
+// if (process.env.ALLOWED_DIMENSIONS) {
+//   const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
+//   dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
+// }
 
 
 exports.handler = (event, context, callback) => {
+  const key = event.queryStringParameters.key;
 
-  const match = event.path.match(/(.*)\/((\d+)x(\d+))\/(.*)/);
-  
-  const dimensions = match[2];
-  const width = parseInt(match[3], 10);
-  const height = parseInt(match[4], 10);
-  const originalKey = `${match[1]}/${match[5]}`;
+  const match = key.match(/(.*)\/([s|w|h])(\d+x{0,1}\d+)\/(.*)/);
 
-  console.log(originalKey);
-
-  if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+  if (!match) {
     callback(null, {
       statusCode: '403',
       headers: {},
-      body: '',
+      body: 'Wrong file name',
     });
     return;
   }
 
+  // requested resize direction 
+  // s: width and height | w: width | h: height
+  let direction = match[2];
+  // requested size
+  let size = match[3];
+
+  let filename = match[4];
+  let folder = match[1];
+
+  // determine widht & height for resize
+  let width, height;
+
+  switch (direction) {
+    case 's':
+      width = parseInt(size.split('x')[0], 10);
+      height = parseInt(size.split('x')[1], 10);
+      break;
+    case 'w':
+      width = parseInt(size, 10);
+      height = null;
+      break;
+    case 'h':
+      width = null;
+      height = parseInt(size, 10);
+      break;
+    default:
+      callback(null, {
+        statusCode: '403',
+        headers: {},
+        body: 'Wrong size',
+      });
+      return;
+      break;
+  }
+
+  // determine file format
+  let extension = filename.split('.')[1];
+  let requiredFormat = extension == "jpg" ? "jpeg" : extension;
+
+  // set original jpg filename
+  let requiredFile = filename.split('.')[0] + '.jpg';
+
+  // original file in the bucket
+  let originalKey = `${folder}/${requiredFile}`;
+
+  // check only allowed sizes
+  //
+  // if (ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
+  //   callback(null, {
+  //     statusCode: '403',
+  //     headers: {},
+  //     body: '',
+  //   });
+  //   return;
+  // }
+
+  // get S3 object
   S3.getObject({
       Bucket: BUCKET,
       Key: originalKey
     }).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
+      .toFormat(requiredFormat)
       .toBuffer()
     )
     .then(buffer => S3.putObject({
-      Body: buffer,
-      Bucket: BUCKET,
-      ContentType: 'image/png',
-      Key: key,
-    }).promise())
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: 'image/' + requiredFormat,
+        CacheControl: 'max-age=31536000',
+        Key: key,
+        StorageClass: 'STANDARD'
+      }).promise()
+      .catch(err => callback(err)))
     .then(() => callback(null, {
       statusCode: '301',
       headers: {
@@ -60,105 +115,4 @@ exports.handler = (event, context, callback) => {
       body: '',
     }))
     .catch(err => callback(err))
-
-  // *****************************************************
-  // let response = event.Records[0].cf.response;
-
-  // console.log("Response status code :%s", response.status);
-
-  // //check if image is not present
-  // if (response.status == 404) {
-
-  //   let request = event.Records[0].cf.request;
-  //   let params = querystring.parse(request.querystring);
-
-  //   // if there is no dimension attribute, just pass the response
-  //   if (!params.d) {
-  //     callback(null, response);
-  //     return;
-  //   }
-
-  //   // read the dimension parameter value = width x height and split it by 'x'
-  //   let dimensionMatch = params.d.split("x");
-
-  //   // read the required path. Ex: uri /images/100x100/webp/image.jpg
-  //   let path = request.uri;
-
-  //   // read the S3 key from the path variable.
-  //   // Ex: path variable /images/100x100/webp/image.jpg
-  //   let key = path.substring(1);
-
-  //   // parse the prefix, width, height and image name
-  //   // Ex: key=images/200x200/webp/image.jpg
-  //   let prefix, originalKey, match, width, height, requiredFormat, imageName;
-  //   let startIndex;
-
-  //   try {
-  //     match = key.match(/(.*)\/(\d+)x(\d+)\/(.*)\/(.*)/);
-  //     prefix = match[1];
-  //     width = parseInt(match[2], 10);
-  //     height = parseInt(match[3], 10);
-
-  //     // correction for jpg required for 'Sharp'
-  //     requiredFormat = match[4] == "jpg" ? "jpeg" : match[4];
-  //     imageName = match[5];
-  //     originalKey = prefix + "/" + imageName;
-  //   } catch (err) {
-  //     // no prefix exist for image..
-  //     console.log("no prefix present..");
-  //     match = key.match(/(\d+)x(\d+)\/(.*)\/(.*)/);
-  //     width = parseInt(match[1], 10);
-  //     height = parseInt(match[2], 10);
-
-  //     // correction for jpg required for 'Sharp'
-  //     requiredFormat = match[3] == "jpg" ? "jpeg" : match[3];
-  //     imageName = match[4];
-  //     originalKey = imageName;
-  //   }
-
-  //   // get the source image file
-  //   S3.getObject({
-  //       Bucket: BUCKET,
-  //       Key: originalKey
-  //     }).promise()
-  //     // perform the resize operation
-  //     .then(data => Sharp(data.Body)
-  //       .resize(width, height)
-  //       .toFormat(requiredFormat)
-  //       .toBuffer()
-  //     )
-  //     .then(buffer => {
-  //       // save the resized object to S3 bucket with appropriate object key.
-  //       S3.putObject({
-  //           Body: buffer,
-  //           Bucket: BUCKET,
-  //           ContentType: 'image/' + requiredFormat,
-  //           CacheControl: 'max-age=31536000',
-  //           Key: key,
-  //           StorageClass: 'STANDARD'
-  //         }).promise()
-  //         // even if there is exception in saving the object we send back the generated
-  //         // image back to viewer below
-  //         .catch(() => {
-  //           console.log("Exception while writing resized image to bucket")
-  //         });
-
-  //       // generate a binary response with resized image
-  //       response.status = 200;
-  //       response.body = buffer.toString('base64');
-  //       response.bodyEncoding = 'base64';
-  //       response.headers['content-type'] = [{
-  //         key: 'Content-Type',
-  //         value: 'image/' + requiredFormat
-  //       }];
-  //       callback(null, response);
-  //     })
-  //     .catch(err => {
-  //       console.log("Exception while reading source image :%j", err);
-  //     });
-  // } // end of if block checking response statusCode
-  // else {
-  //   // allow the response to pass through
-  //   callback(null, response);
-  // }
 };
